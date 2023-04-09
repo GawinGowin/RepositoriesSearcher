54699cb5e5a1431c02a6ad0a1bfc7b9c2143e491
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2023-04-10 03:14:41 +0900

    add textBox

--- lib/pages/results.dart
+++ lib/pages/results.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
-class Results extends StatelessWidget {
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:repo_searcher/providers.dart';
+
+class Results extends ConsumerWidget {
   const Results({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final inputedText = ref.watch(inputFieldProvider);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Results'),
+        title: Text('Results'),
       ),
+      body: Text('$inputedText'),
     );
   }
 }
--- lib/providers.dart
+++ lib/providers.dart
@@ -1,3 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+//RootPage
 final indexProvider = StateProvider((ref) => 0);
+
+//HomePage
+final inputFieldProvider = StateProvider((ref) => "");
