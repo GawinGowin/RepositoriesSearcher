9c467d12e2f644e5b5136283f59cd583b4d5f422
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2023-04-10 01:42:36 +0900

    fix type miss

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,10 +10,11 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:repo_searcher/main.dart';
 
+
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Root());
+    await tester.pumpWidget(const Route());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
