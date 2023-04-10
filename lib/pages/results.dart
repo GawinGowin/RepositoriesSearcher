64dc4a456bc49f82076833af7e4d64ba5525eea5
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2023-04-11 06:46:19 +0900

    update use AsyncNotifier

--- lib/pages/results.dart
+++ lib/pages/results.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:repo_searcher/providers/providers.dart';
+import 'package:repo_searcher/providers/dataProviders.dart';
 
 import 'package:repo_searcher/modules/itemCard.dart';
-
 import 'package:repo_searcher/utils/responseData.dart';
-
-const String host = "api.github.com";
-const String path = '/search/repositories';
 
 class Results extends ConsumerWidget {
   Results({super.key});
@@ -18,25 +13,32 @@ class Results extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final inputField = ref.watch(searchProvider);
-    final itemList = ref.watch(itemsProvider);
     final responseList = ref.watch(responseProvider);
 
-    getData(ref, inputField);
+    final itemList = ref.watch(dataNotifierProvider);
+    final itemListModule = itemList.when(
+      loading: () => const Text('準備中...'),
+      error: (e, s) => Text('エラー $e'),
+      data: (d) => Text('取得したデータが存在'),
+    );
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Total: ${responseList["total_count"]} Repositories'),
         leading: IconButton(
         icon: const Icon(Icons.arrow_back),
-        onPressed: () {
+        onPressed: () { // Homeに戻る際に情報をリセット
           ref.read(responseProvider.notifier).state = {};
-          ref.read(itemsProvider.notifier).state = [];
+
+          final dataNotifier = ref.read(dataNotifierProvider.notifier);
+          dataNotifier.resetState();
           Navigator.pop(context, );
         },
       )
       ),
 
-      body: 
+      body: itemListModule,
+        /*
         Container(
           child: ListView.builder(
             padding: const EdgeInsets.all(16),
@@ -46,51 +48,14 @@ class Results extends ConsumerWidget {
             }
           ),
         ),
+        */
 
       floatingActionButton: FloatingActionButton( //仮
         onPressed: (){
-          getData(ref, inputField);
+          //getData(ref, inputField);
         },
         child: const Icon(Icons.sync),
       ),
     );
-  }
-}
-
-Future<void> getData(ref, inputField) async {
-  try {
-    var response = await http.get(Uri.https(host, path, inputField));
-    var responseJson = checkResponse(response);
-    
-    int count = responseJson["total_count"];
-    bool incomplete_results = responseJson["incomplete_results"];
-    ref.read(responseProvider.notifier).state = {"total_count": count, "incomplete_results": incomplete_results};
-
-    List items = responseJson["items"];
-    List formedItems = items.map((e) => cleanData(e)).toList();
-
-    ref.read(itemsProvider.notifier).state = formedItems;
-    print("$count, $incomplete_results");
-    return;
-  } catch (_) {
-  }
-}
-
-dynamic checkResponse(http.Response response) {
-  switch (response.statusCode) {
-    case 200:
-      return jsonDecode(response.body);
-    case 400:
-      throw Exception('一般的なクライアントエラーです');
-    case 401:
-      throw Exception('アクセス権限がない、または認証に失敗しました');
-    case 403:
-      throw Exception('閲覧権限がないファイルやフォルダです');
-    case 404:
-      throw Exception('404 Not Found');
-    case 500:
-      throw Exception('何らかのサーバー内で起きたエラーです');
-    default:
-      throw Exception('何かしらの問題が発生しています');
   }
 }
