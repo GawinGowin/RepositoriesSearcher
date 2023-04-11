e5443ae721f90517435789bd347c6b876a51ea7d
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2023-04-11 22:51:02 +0900

    flutter_riverpod -> hooks_riverpod

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:repo_searcher/providers/home_providers.dart';
 import 'package:repo_searcher/pages/results.dart';
