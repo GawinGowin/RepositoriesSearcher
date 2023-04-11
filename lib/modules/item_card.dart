bf586e47a8f2797456e94022e5805c56963efc11
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2023-04-12 01:07:13 +0900

    update widget : Scrollable

--- lib/modules/item_card.dart
+++ lib/modules/item_card.dart
@@ -23,48 +23,48 @@ Widget ItemCard(context, index, itemList) {
             child: Image.network('${itemList[index].avatar_url}',)
           ),
 
-          content: Column(
-            children: [   
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 10),
-                child: Text(
-                  '${itemList[index].name}',
-                  style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.5),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [   
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    '${itemList[index].name}',
+                    style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.5),
+                  ),
                 ),
-              ),
-              // https://primer.style/design/foundations/icons/
-              Table(
-                children: [
-                  tablePartial(
-                    const Text('Language'),
-                    Text('${itemList[index].language=="" ? "None": itemList[index].language}')
-                  ),                      
-                  tablePartial(
-                    const Icon(Icons.star_border),
-                    Text('${itemList[index].stargazers_count}'),
-                  ),
-                  tablePartial(
-                    const Icon(Icons.visibility),
-                    Text('${itemList[index].watchers_count}')
-                  ),
-                  tablePartial(
-                    const Icon(Icons.lan_outlined),
-                    Text('${itemList[index].forks_count}')
-                  ),
-                  tablePartial(
-                    const Icon(Icons.adjust),
-                    Text('${itemList[index].open_issues_count}')
-                  ),       
-                ],
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 5),
-                alignment: Alignment.centerLeft,
-                child: const Text('description')
-              ),
-
-              Expanded(
-                child: Container(
+                // https://primer.style/design/foundations/icons/
+                Table(
+                  children: [
+                    tablePartial(
+                      const Text('Language'),
+                      Text('${itemList[index].language=="" ? "None": itemList[index].language}')
+                    ),                      
+                    tablePartial(
+                      const Icon(Icons.star_border),
+                      Text('${itemList[index].stargazers_count}'),
+                    ),
+                    tablePartial(
+                      const Icon(Icons.visibility),
+                      Text('${itemList[index].watchers_count}')
+                    ),
+                    tablePartial(
+                      const Icon(Icons.lan_outlined),
+                      Text('${itemList[index].forks_count}')
+                    ),
+                    tablePartial(
+                      const Icon(Icons.adjust),
+                      Text('${itemList[index].open_issues_count}')
+                    ),       
+                  ],
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 5),
+                  alignment: Alignment.centerLeft,
+                  child: const Text('description')
+                ),
+          
+                Container(
                   width: double.infinity,
                   padding: const EdgeInsets.only(top: 5),
                   decoration: const BoxDecoration(
@@ -72,13 +72,11 @@ Widget ItemCard(context, index, itemList) {
                       top: BorderSide(color: Colors.grey)
                     ),
                   ),
-                  child: SingleChildScrollView(
-                    child: Text('${itemList[index].description}')
-                  ),
+                  child: Text('${itemList[index].description}')
                 )
-   
-              )
-            ],
+          
+              ],
+            ),
           ),
           
           actions: <Widget>[
