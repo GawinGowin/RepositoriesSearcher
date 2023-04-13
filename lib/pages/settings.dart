cccee425e8e739593f535ca7a2efcff7417a7b54
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2023-04-13 18:58:58 +0900

    add thememode change

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:repo_searcher/modules/setting_panel.dart';
+import 'package:repo_searcher/providers/inputs_providers.dart';
 
-class Settings extends StatelessWidget {
+class Settings extends ConsumerWidget {
   const Settings({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    var modeNow = ref.watch(themeNotifierProvider);
+    if (modeNow == ThemeMode.system){
+      bool isDarkMode = WidgetsBinding.instance.window.platformBrightness == Brightness.dark;
+      modeNow = isDarkMode ? ThemeMode.dark : ThemeMode.light;
+    }
+    var switchIcon = modeNow == ThemeMode.light ? Icons.dark_mode : Icons.light_mode;
+    var modeNext = modeNow == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Settings'),
+        actions: [
+          IconButton(
+            icon: Icon(switchIcon),
+            onPressed: () {
+              ref.read(themeNotifierProvider.notifier).updateState(modeNext);
+            },
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
